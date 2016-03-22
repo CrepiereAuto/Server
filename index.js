65864a22005566f0c600e8d73f71a373ae0704d2
Hougo13 <hougo13@gmail.com>
2016-03-22 21:43:06 +0100

    Add Soft compile

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var manager = require('./lib/manager')
+var update = require('./lib/updater')
 
 http.listen(3030, function(){console.log('Listening *:3030');});
 
 app.get('/', function(req, res){
   res.send('Hello World !');
+});
+
+app.get('/update/soft', function(req, res){
+  res.send('ok');
+  update.soft(function () {
+    console.log('done');
+  })
+});
+
+app.get('/update/app', function(req, res){
+  res.send('ok');
+});
+
+app.get('/update/server', function(req, res){
+  res.send('ok');
+  update.server(function () {
+    console.log('done');
+    proccess.exit(0)
+  })
 });
 
 io.on('connection', function(socket){
