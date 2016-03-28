1263d7430d70a0d3bb30530fa772da85d254dee8
Hougo13 <hougo13@gmail.com>
2016-03-28 15:05:58 +0200

    Add semicolons

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var manager = require('./lib/manager')
-var update = require('./lib/updater')
+var manager = require('./lib/manager');
+var update = require('./lib/updater');
 
 var updating = {
   server: false,
   soft: false,
   app: false
-}
+};
 
 http.listen(3030, function(){console.log('Listening *:3030');});
 
 app.get('/', function(req, res){
-  var page = 'Hello World ! <br>'
-  if(updating['server']){
-    page += '<li>Server</li>'
+  var page = 'Hello World ! <br>';
+  if(updating.server){
+    page += '<li>Server</li>';
   }
-  if(updating['soft']){
-    page += '<li>Soft</li>'
+  if(updating.soft){
+    page += '<li>Soft</li>';
   }
-  if(updating['app']){
-    page += '<li>App</li>'
+  if(updating.app){
+    page += '<li>App</li>';
   }
   res.send(page);
 });
@@ -29,50 +29,50 @@ app.get('/', function(req, res){
 app.all('/update/:key?', function(req, res, next){
   var key = req.params.key;
   if (key == 'server' || key == 'soft' || key == 'app') {
-    updating[key] = true
-    res.redirect('/')
-    next()
+    updating[key] = true;
+    res.redirect('/');
+    next();
   }
 }, function (req, res) {
   var key = req.params.key;
   console.log('Updating '+key);
   update(key, function () {
-    updating[key] = false
+    updating[key] = false;
     console.log(key+' update done');
     if (key == 'server') {
-      restart()
+      restart();
     }
-  })
+  });
 });
 
 io.on('connection', function(socket){
   socket.on('disconnect', function(){
-    manager.stop(socket.id)
-  })
+    manager.stop(socket.id);
+  });
 
   socket.on('start', function(data) {
-    manager.start(socket, data)
-  })
+    manager.start(socket, data);
+  });
 
   socket.on('open', function (data) {
-    manager.open(socket, data)
-  })
+    manager.open(socket, data);
+  });
 
   socket.on('join', function (data) {
-    manager.join(socket, data)
-  })
+    manager.join(socket, data);
+  });
 
   socket.on('update', function (data) {
-    manager.update(socket, data)
-  })
+    manager.update(socket, data);
+  });
 
-})
+});
 
 function restart(){
   if (updating == {server: false, soft: false, app: false}) {
-    process.exit(0)
+    process.exit(0);
   }else {
     console.log('other update is runnig');
-    setTimeout(restart, 1000)
+    setTimeout(restart, 1000);
   }
 }
